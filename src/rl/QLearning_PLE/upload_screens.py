850ba29d096a4b84be3020b9a7476acefab52eb5
dkopp <damian.kopp@epfl.ch>
2025-05-31 19:32:44 +0200

    Commit latest local modifications before pulling merge

--- src/rl/QLearning_PLE/upload_screens.py
+++ src/rl/QLearning_PLE/upload_screens.py
@@ -4,10 +4,10 @@ from huggingface_hub import upload_large_folder, HfApi
 
 def upload_screens():
     upload_large_folder(
-        folder_path="screens",  # path to your local folder
-        repo_id="DiffusionArcade/Pong",
+        folder_path="screens2",  # path to your local folder
+        repo_id="DiffusionArcade/Pong_ep4999_allsteps_30fps",
         repo_type="dataset",              # specify this is a dataset repo
-        allow_patterns=["images/damian_screens_03/**.jpg"]
+        allow_patterns=["images/damian_screens_02/**.jpg"]
         # allow_patterns=["*.jpg", "*.png"], # optional: only include image files
         # ignore_patterns=["screens/*.jpg"]
         # ignore_patterns=None,             # optional: skip files
